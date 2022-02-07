e4932e12469f1ba5fd92e245b3d82f17c96057e6
Franklin "Snaipe" Mathieu <snaipe@arista.com>
2022-02-07 09:34:06 +0100

    encoding: add customizable Encoders and Decoders mappings
    This allows users to register custom configuration extensions for the
    Encoder and Decoder types.

--- config.go
+++ config.go
@@ -8,14 +8,44 @@ package boa
 import (
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
 
 	"snai.pe/boa/encoding"
 	"snai.pe/boa/encoding/json5"
 	"snai.pe/boa/encoding/toml"
 )
+
+// Decoders map filename extensions to decoders.  By default, the following
+// coniguration languages are associated with the extensions:
+//
+//     - JSON5: .json and .json5
+//     - TOML: .toml
+//
+// The contents of this map controls how the Decoder type deduces which
+// decoder to use based on the file extension of the input.
+var Decoders = map[string]func(io.Reader) encoding.Decoder {
+	".toml":  toml.NewDecoder,
+	".json5": json5.NewDecoder,
+	".json":  json5.NewDecoder,
+}
+
+// Encoders map filename extensions to encoders.  By default, the following
+// coniguration languages are associated with the extensions:
+//
+//     - JSON5: .json and .json5
+//     - TOML: .toml
+//
+// The contents of this map controls how the Encoder type deduces which
+// encoder to use based on the file extension of the output.
+var Encoders = map[string]func(io.Writer) encoding.Encoder {
+	".toml":  toml.NewEncoder,
+	".json5": json5.NewEncoder,
+	".json":  json5.NewEncoder,
+}
 
 // A Decoder reads and decodes a configuration from an input file.
 type Decoder struct {
@@ -26,13 +56,11 @@ type Decoder struct {
 // NewDecoder returns a new Decoder that reads from `in`.
 //
 // The configuration language of the input file is deduced based on the
-// file extension of its file path:
+// file extension of its file path.  The file-extension-to-decoder mapping
+// is defined by the contents of the Decoders map in this package.
 //
-//     - JSON5: .json and .json5
-//     - TOML: .toml
-//
-// Custom file extensions are not supported, and one of the decoders in
-// snai.pe/boa/encoding must be used instead.
+// To only use one specific configuration language, do not use this decoder:
+// Use instead the decoder for the chosen language in snai.pe/boa/encoding.
 func NewDecoder(in encoding.StatableReader) *Decoder {
 	return &Decoder{in: in, opts: append(([]interface{})(nil), defaultDecoderOptions...)}
 }
@@ -66,22 +94,23 @@ func (dec *Decoder) Decode(v interface{}) error {
 		}
 		ext := filepath.Ext(name)
 
-		var decoder encoding.Decoder
-		switch ext {
-		case ".json", ".json5":
-			decoder = json5.NewDecoder(in)
-		case ".toml":
-			decoder = toml.NewDecoder(in)
-		default:
+		decoder, ok := Decoders[ext]
+		if !ok {
 			return fmt.Errorf("no known decoder for file extension %q", ext)
 		}
-		return decoder.Option(dec.opts...).Decode(v)
+		return decoder(in).Option(dec.opts...).Decode(v)
 	}
 
 	switch in := dec.in.(type) {
 	case *FileSet:
 		for {
-			if err := in.Next(".toml", ".json5", ".json"); err != nil {
+			keys := make([]string, 0, len(Decoders))
+			for k, _ := range Decoders {
+				keys = append(keys, k)
+			}
+			sort.Strings(keys)
+
+			if err := in.Next(keys...); err != nil {
 				if err == os.ErrNotExist {
 					break
 				}
@@ -108,14 +137,12 @@ type Encoder struct {
 
 // NewEncoder returns a new encoder that writes into `out`.
 //
-// The configuration language of the output file is deduced based on the
-// file extension of its file path:
+// The configuration language of the input file is deduced based on the
+// file extension of its file path.  The file-extension-to-decoder mapping
+// is defined by the contents of the Decoders map in this package.
 //
-//     - JSON5: .json and .json5
-//     - TOML: .toml
-//
-// Custom file extensions are not supported, and one of the decoders in
-// snai.pe/boa/encoding must be used instead.
+// To only use one specific configuration language, do not use this encoder:
+// Use instead the encoder for the chosen language in snai.pe/boa/encoding.
 func NewEncoder(out encoding.StatableWriter) *Encoder {
 	return &Encoder{out: out, opts: append(([]interface{})(nil), defaultEncoderOptions...)}
 }
@@ -147,16 +174,11 @@ func (enc *Encoder) Encode(v interface{}) error {
 	}
 	ext := filepath.Ext(name)
 
-	var encoder encoding.Encoder
-	switch ext {
-	case ".json", ".json5":
-		encoder = json5.NewEncoder(enc.out)
-	case ".toml":
-		encoder = toml.NewEncoder(enc.out)
-	default:
+	encoder, ok := Encoders[ext]
+	if !ok {
 		return fmt.Errorf("no known decoder for file extension %q", ext)
 	}
-	return encoder.Option(enc.opts...).Encode(v)
+	return encoder(enc.out).Option(enc.opts...).Encode(v)
 }
 
 // Load loads the configuration files for the specified name into the specified
