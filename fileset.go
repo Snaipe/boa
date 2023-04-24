2aa7e8aa162eaa0edc6fecd46394995ed8ff86b0
Wade Carpenter <wade@arista.com>
2023-04-24 15:45:34 -0700

    fileset: support multiple names in a new `OpenMultiple` method
    The primary use case is to provide a notion of precedence in
    configuration files. This will allow us to have, for example, a "base"
    configuration and then an "override" configuration. In the simple
    example:
    
        /etc/base.toml
        /etc/override.toml
    
    If we call `OpenMultiple({"base", "override"}, {"/etc"})` then we'll
    first apply the configuration in `/etc/base.toml` and then the
    configuration in `/etc/override.toml` which will allow us to override
    parts of the base configuration in a consistent and predictable way.

--- fileset.go
+++ fileset.go
@@ -6,7 +6,9 @@
 package boa
 
 import (
+	"embed"
 	"errors"
+	"fmt"
 	"io"
 	"io/fs"
 	"os"
@@ -17,11 +19,13 @@ import (
 // FileSet represents the combined set of configuration files with a specific
 // name, that are contained within any of the directories in a search path.
 type FileSet struct {
-	fs     []fs.FS
-	name   string
-	index  int
-	opened fs.File
-	closed bool
+	fs        []fs.FS
+	fsIndex   int
+	names     []string
+	nameIndex int
+	opened    fs.File
+	closed    bool
+	used      []string
 }
 
 // Open opens a set of configuration files by name.
@@ -39,16 +43,36 @@ type FileSet struct {
 // specified extension. Conversely, names without an extension will match all
 // files whose stem is the last path component of the filename.
 func Open(name string, paths ...fs.FS) *FileSet {
-	if filepath.IsAbs(name) {
-		panic("Open does not take absolute paths; use os.Open instead.")
-	}
-	if strings.HasPrefix(name, "."+string(filepath.Separator)) || strings.HasPrefix(name, ".."+string(filepath.Separator)) {
-		panic("Open does not take cwd-relative paths; use os.Open instead.")
-	}
+	checkNames(name)
 	if paths == nil {
 		paths = ConfigPaths()
 	}
-	return &FileSet{fs: paths, name: name}
+	return &FileSet{fs: paths, names: []string{name}}
+}
+
+// OpenMultiple opens a set of configuration files with one or more names.
+//
+// The order of the `names` slice defines a precedence of files across all
+// search paths. With a single `name`, the behaviour is identical to `Open`.
+//
+// The iteration order is described in detail in `Next()`.
+func OpenMultiple(names []string, paths []fs.FS) *FileSet {
+	checkNames(names...)
+	if paths == nil {
+		paths = ConfigPaths()
+	}
+	return &FileSet{fs: paths, names: names}
+}
+
+func checkNames(names ...string) {
+	for _, name := range names {
+		if filepath.IsAbs(name) {
+			panic("Open does not take absolute paths; use os.Open instead.")
+		}
+		if strings.HasPrefix(name, "."+string(filepath.Separator)) || strings.HasPrefix(name, ".."+string(filepath.Separator)) {
+			panic("Open does not take cwd-relative paths; use os.Open instead.")
+		}
+	}
 }
 
 // Read reads the contents of the currently opened configuration file into
@@ -73,7 +97,20 @@ func (cfg *FileSet) Close() error {
 	if cfg.opened == nil {
 		return nil
 	}
-	return cfg.opened.Close()
+	if err := cfg.opened.Close(); err != nil {
+		return err
+	}
+	cfg.opened = nil
+	return nil
+}
+
+func fsPath(f fs.FS) string {
+	switch f.(type) {
+	case embed.FS:
+		return "(embedded)"
+	default:
+		return fmt.Sprint(f)
+	}
 }
 
 // Next closes the current configuration file if any is opened, and opens
@@ -84,8 +121,20 @@ func (cfg *FileSet) Close() error {
 //
 // If all of the search paths have been exhausted, os.ErrNotExist is returned.
 //
+// When there are multiple names (see OpenMultiple), they will be returned in order
+// first by name and then by path. For instance, if the names slice is "base",
+// "override" and the paths slice is "/etc", "/root/.config" we'll use the ordering:
+//
+//   - /etc/base.<ext>
+//   - /root/.config/base.<ext>
+//   - /etc/override.<ext>
+//   - /root/.config/override.<ext>
+//
+// This ordering of the names slice ensures that later entries take precedence over
+// earlier ones, regardless of which directory they're in.
+
 // The files are matched in the order of the specified exts slice rather than
-// directory (or lexical) order. For insteance, if the extension slice
+// directory (or lexical) order. For instance, if the extension slice
 // is ".json5", ".json" on a path containing <name>.json5 and <name>.json will
 // open <name>.json5 if it exists, or then <name>.json.
 func (cfg *FileSet) Next(exts ...string) error {
@@ -95,23 +144,27 @@ func (cfg *FileSet) Next(exts ...string) error {
 		_ = cfg.opened.Close()
 	}
 
-	for ; cfg.index < len(cfg.fs); cfg.index++ {
-		for _, ext := range exts {
-			realext := filepath.Ext(cfg.name)
-			if realext != "" && realext != ext {
-				continue
+	for ; cfg.nameIndex < len(cfg.names); cfg.nameIndex++ {
+		for ; cfg.fsIndex < len(cfg.fs); cfg.fsIndex++ {
+			for _, ext := range exts {
+				name := cfg.names[cfg.nameIndex]
+				realext := filepath.Ext(name)
+				if realext != "" && realext != ext {
+					continue
+				}
+				stem := name[:len(name)-len(realext)]
+				f, err := cfg.fs[cfg.fsIndex].Open(stem + ext)
+				switch {
+				case errors.Is(err, fs.ErrNotExist):
+					continue
+				case err != nil:
+					return err
+				}
+				cfg.used = append(cfg.used, fmt.Sprintf("%v/%v%v", fsPath(cfg.fs[cfg.fsIndex]), stem, ext))
+				cfg.opened = f
+				cfg.Skip()
+				return nil
 			}
-			stem := cfg.name[:len(cfg.name)-len(realext)]
-			f, err := cfg.fs[cfg.index].Open(stem + ext)
-			switch {
-			case errors.Is(err, fs.ErrNotExist):
-				continue
-			case err != nil:
-				return err
-			}
-			cfg.opened = f
-			cfg.index++
-			return nil
 		}
 	}
 	if cfg.opened == nil {
@@ -120,13 +173,21 @@ func (cfg *FileSet) Next(exts ...string) error {
 		// variables still happen
 
 		cfg.opened = discard
-		cfg.index++
+		cfg.fsIndex++
 		return nil
 	}
 	return os.ErrNotExist
 }
 
-type discardFile struct {}
+// Used returns a slice containing the file names of all files that were opened by
+// calls to Next().
+func (cfg *FileSet) Used() []string {
+	v := make([]string, len(cfg.used))
+	copy(v, cfg.used)
+	return v
+}
+
+type discardFile struct{}
 
 func (discardFile) Read([]byte) (int, error) {
 	return 0, io.EOF
@@ -159,5 +220,9 @@ func (cfg *FileSet) File() fs.File {
 // application would rather warn but continue parsing configurations in the
 // rest of the paths.
 func (cfg *FileSet) Skip() {
-	cfg.index++
+	cfg.fsIndex++
+	if cfg.fsIndex == len(cfg.fs) {
+		cfg.fsIndex = 0
+		cfg.nameIndex++
+	}
 }
